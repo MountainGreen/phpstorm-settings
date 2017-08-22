3537ea90194e5ec15ea64fa5ad148032d9130c5e
Mountain <mountain@192.168.178.108>
2017-08-22 13:56:18 +0200

    PS-2017.2.1 <mountain@elisabeths-macbook-pro.local Create ide.general.xml, _@user_Default.icls, .DS_Store, project.default.xml, Default copy.xml, lumicode.xml, other.xml, Default.xml, wenming.xml, ui.lnf.xml, EyeSaver.xml, _@user_Cobalt.icls, remote-servers.xml, colors.scheme.xml, Anja.icls, databaseDrivers.xml, sht.xml, GitHub.xml, Dreamweaver.xml, laf.xml, Nostalgia.icl, Netbeans.xml, databaseSettings.xml, keymap.xml, editor.xml, Basic Vue File Template.vue, debugger.xml, Github.icls

---
+++ fileTemplates/Basic Vue File Template.vue
@@ -0,0 +1,36 @@
+<!--HTML Template ===================================== -->
+
+<template>
+
+<div class="home">
+
+
+
+</div>
+
+</template>
+
+
+
+<!--JavaScript ======================================== -->
+
+<script>
+
+export default {
+    name: 'home',
+    data () {
+        return {}
+    }
+}
+
+</script>
+
+
+
+<!--Styles ============================================ -->
+
+<style lang="scss" scoped>
+
+
+
+</style>
